6a2bd6ae3ff30e37caf9c1b192f7f17e4e9c2b43
CaioMBM <caiombmonteiro@gmail.com>
2024-05-14 15:01:08 -0300

    Estilizei as pages dos links com"Em Construção"

--- js/header-redirecionar.js
+++ js/header-redirecionar.js
@@ -1,21 +1,17 @@
 // Leva para página Help Center
 const linkHelpCenter = document.querySelector('#help-link')
-
 linkHelpCenter.addEventListener('click', () => {
     window.open('../pages/page1.html', '_self')
 })
 
 // Leva para outra página (que ainda será criada)
 const btnSubmitRequest = document.querySelector('#btn-submit')
-
 btnSubmitRequest.addEventListener('click', () => {
     window.open('../pages/page2.html', '_self')
 })
 
 // Leva para outra página (que ainda será criada)
 const btnSignIn = document.querySelector('#btn-sign')
-
 btnSignIn.addEventListener('click', () => {
     window.open('../pages/page3.html', '_self')
 })
-
