5c02c147ff665999eb3171a62e27308a9b8562f2
CaioMBM <caiombmonteiro@gmail.com>
2024-05-14 15:15:41 -0300

    Linkando os links do header
    - Deletei os pages 1,2 e 4 e centralizei em um arquivo ("construcao.html");
    - Posteriormente criar as pages de cada link corretamente.

--- js/header-redirecionar.js
+++ js/header-redirecionar.js
@@ -1,17 +1,11 @@
-// Leva para página Help Center
-const linkHelpCenter = document.querySelector('#help-link')
-linkHelpCenter.addEventListener('click', () => {
-    window.open('../pages/page1.html', '_self')
-})
-
-// Leva para outra página (que ainda será criada)
+// Leva para página do SUBMIT
 const btnSubmitRequest = document.querySelector('#btn-submit')
 btnSubmitRequest.addEventListener('click', () => {
-    window.open('../pages/page2.html', '_self')
+    window.open('pages/construcao.html', '_self')
 })
 
-// Leva para outra página (que ainda será criada)
+// Leva para página para LOGAR
 const btnSignIn = document.querySelector('#btn-sign')
 btnSignIn.addEventListener('click', () => {
-    window.open('../pages/page3.html', '_self')
+    window.open('pages/construcao.html', '_self')
 })
