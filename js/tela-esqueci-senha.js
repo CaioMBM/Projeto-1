959d0709bc88b5f78988c77cb41a4f2365f6312e
CaioMBM <caiombmonteiro@gmail.com>
2024-05-17 18:08:22 -0300

    Finalizei a tela de esqueci a senha
    - Com JS: Fiz a validação do email;
    - Com JS: Fiz o modal aparece caso o email sejá inválido;
    - Com JS: Fiz o redirecionamento (como não sei p/qual tela vai no site original, então coloquei na tela "em construção" que eu criei localmente)

---
+++ js/tela-esqueci-senha.js
@@ -0,0 +1,15 @@
+// Ao clicar no submit - VALIDAR
+function validarEmail(){
+    var email = document.getElementById('ipt-email').value
+    const modalEmailInvalido = document.getElementById('email-invalido')
+    
+    // Email válido
+    if (email == "caio@gmail.com"){
+        // Redireciona
+        location.href = "construcao.html";
+    }
+    // Email inválido
+    else{
+        modalEmailInvalido.style.display = "block";
+    }
+}
