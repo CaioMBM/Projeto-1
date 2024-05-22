0883d1504589ae5fd296994212a3664d61bd1be7
CaioMBM <caiombmonteiro@gmail.com>
2024-05-21 21:55:16 -0300

    Criando a tela de submit request - header feito (JS)
    - Faltava linkar o botar "Sign In", fiz agora com JS;
    - Precisei alterar o id dos buttons do header.

--- js/header-redirecionar.js
+++ js/header-redirecionar.js
@@ -1,3 +1,4 @@
+// TELA - index.html
 // Leva para página do SUBMIT
 const btnSubmitRequest = document.querySelector('#btn-submit')
 btnSubmitRequest.addEventListener('click', () => {
@@ -9,3 +10,19 @@ const btnSignIn = document.querySelector('#btn-sign')
 btnSignIn.addEventListener('click', () => {
     window.open('pages/login1.html', '_self')
 })
+
+
+// TELA - suporte.html
+// Leva para página do SUBMIT
+function buttonSubmitRequest(){
+    location.href = 'suporte.html'
+    
+    console.log('clicou submit')
+}
+
+// Leva para página para LOGAR
+function buttonSignIn(){
+    location.href = 'login1.html'
+    
+    console.log('clicou login')
+}
