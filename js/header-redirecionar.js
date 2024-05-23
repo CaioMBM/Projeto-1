d53c75807b5d1a8f9713a07b953396d033381e03
CaioMBM <caiombmonteiro@gmail.com>
2024-05-23 19:32:27 -0300

    Correção do botão "Help" e linkagem do botão submit.
    - Alterei o símbolo de interrogação do botão "Help" para um svg;
    - JS: Na page "Index", ao clicar no botão de submit, no header, será levado para a tela respectiva.

--- js/header-redirecionar.js
+++ js/header-redirecionar.js
@@ -2,7 +2,7 @@
 // Leva para página do SUBMIT
 const btnSubmitRequest = document.querySelector('#btn-submit')
 btnSubmitRequest.addEventListener('click', () => {
-    window.open('pages/construcao.html', '_self')
+    window.open('pages/suporte.html', '_self')
 })
 
 // Leva para página para LOGAR
@@ -16,13 +16,9 @@ btnSignIn.addEventListener('click', () => {
 // Leva para página do SUBMIT
 function buttonSubmitRequest(){
     location.href = 'suporte.html'
-    
-    console.log('clicou submit')
 }
 
 // Leva para página para LOGAR
 function buttonSignIn(){
     location.href = 'login1.html'
-    
-    console.log('clicou login')
 }
